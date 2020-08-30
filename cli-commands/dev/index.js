7ff852cf21009c258346ef562b6cda0ef136b908
Jonithan <695993410@qq.com>
2020-08-30 21:49:59 +0800

    docs: 补充配置优先级设置

--- cli-commands/dev/index.js
+++ cli-commands/dev/index.js
@@ -4,16 +4,14 @@ const path = require("path");
 
 module.exports = function (optionObj, devWebpackPath) {
   console.log(optionObj);
-
   const config = require(devWebpackPath);
-  const contentBase = path.join(process.cwd());
-  console.log(contentBase)
+  const devServerOptions = Object.assign({
+    stats: 'normal'
+  }, config.devServer);
+  // TODO 配置来源：webpack.dev.js与脚手架输入及部分默认
+  // 对于各配置，优先级为 手动输入 > 脚手架 > 默认
 
-  const devServerOptions = Object.assign({}, config.devServer)
-  console.log(devServerOptions);
-  
   WebpackDevServer.addDevServerEntrypoints(config, devServerOptions);
-
   const compiler = Webpack(config);
   const server = new WebpackDevServer(compiler, devServerOptions);
 
@@ -21,3 +19,22 @@ module.exports = function (optionObj, devWebpackPath) {
     console.log("Starting server on http://localhost:8080");
   });
 };
+
+{
+  hot: true,
+    host: 'localhost',
+      port: 8081,
+        historyApiFallback: true,
+          proxy: { },
+  publicPath: '/',
+    filename: 'main-[hash].js',
+      hotOnly: undefined,
+        clientLogLevel: 'info',
+          stats: {
+    cached: false,
+      cachedAssets: false,
+        colors: { level: 3, hasBasic: true, has256: true, has16m: true }
+  },
+  open: true,
+    openPage: ''
+}
